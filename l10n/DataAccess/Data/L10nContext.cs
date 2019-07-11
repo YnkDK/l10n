aecf021d3e8f46df5129196bf7d19abb9bef154c
Martin Storgaard <ms@ynk.gl>
2019-07-11 20:48:11 +0200

    Added context

---
+++ l10n/DataAccess/Data/L10nContext.cs
@@ -0,0 +1,32 @@
+﻿using DataAccess.Models;
+using JetBrains.Annotations;
+using Microsoft.EntityFrameworkCore;
+using NJsonSchema.Annotations;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Data
+{
+    public class L10nContext : DbContext
+    {
+        public L10nContext([NotNull] DbContextOptions<L10nContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Locale> Locales { get; set; }
+        public DbSet<Localization> Localizations { get; set; }
+        public DbSet<Organization> Organizations { get; set; }
+        public DbSet<Phrase> Phrases { get; set; }
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Locale>().ToTable(nameof(Locale));
+            modelBuilder.Entity<Localization>().ToTable(nameof(Localization));
+            modelBuilder.Entity<Organization>().ToTable(nameof(Organization));
+            modelBuilder.Entity<Phrase>().ToTable(nameof(Phrase));
+            modelBuilder.Entity<User>().ToTable(nameof(User));
+        }
+    }
+}
